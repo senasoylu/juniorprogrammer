090a467dd498568022fdc91e9d1ba33758e45233
senasoylu <sena.soylu@roofstacks.com>
2025-01-02 11:18:27 +0300

    Player Control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   private float speed = 5.0f;
+    private float turnspeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      //this is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move the vehicle forward
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+       //we turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
+    }
+}
