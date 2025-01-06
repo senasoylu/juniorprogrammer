917b70709275617b803730c85619e2c294d16179
senasoylu <sena.soylu@roofstacks.com>
2025-01-06 13:56:57 +0300

    Unit2-Challenge Completed

---
+++ Assets/Course Library/Scripts/PlayerFarmerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFarmerController : MonoBehaviour
+{
+    public float horizantalInput;
+    public float speed = 10.0f;
+    public float xRange= 10.0f;
+    public GameObject projectilePrefab;
+    
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right *horizantalInput*Time.deltaTime*speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
+        }
+        
+    }
+}
